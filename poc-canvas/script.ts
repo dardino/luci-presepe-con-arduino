d32458869d82a26b8afcbb797d6631252f5cf1ef
Fabio Brunori <fabiobunori@hotmail.it>
2019-11-23 19:22:38 +0100

    funziona

--- poc-canvas/script.ts
+++ poc-canvas/script.ts
@@ -1,30 +1,30 @@
 const EasingFunctions = {
     // no easing, no acceleration
-    linear: function (t) { return t },
+    linear: function (t: number) { return t },
     // accelerating from zero velocity
-    easeInQuad: function (t) { return t*t },
+    easeInQuad: function (t: number) { return t*t },
     // decelerating to zero velocity
-    easeOutQuad: function (t) { return t*(2-t) },
+    easeOutQuad: function (t: number) { return t*(2-t) },
     // acceleration until halfway, then deceleration
-    easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+    easeInOutQuad: function (t: number) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
     // accelerating from zero velocity 
-    easeInCubic: function (t) { return t*t*t },
+    easeInCubic: function (t: number) { return t*t*t },
     // decelerating to zero velocity 
-    easeOutCubic: function (t) { return (--t)*t*t+1 },
+    easeOutCubic: function (t: number) { return (--t)*t*t+1 },
     // acceleration until halfway, then deceleration 
-    easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+    easeInOutCubic: function (t: number) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
     // accelerating from zero velocity 
-    easeInQuart: function (t) { return t*t*t*t },
+    easeInQuart: function (t: number) { return t*t*t*t },
     // decelerating to zero velocity 
-    easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+    easeOutQuart: function (t: number) { return 1-(--t)*t*t*t },
     // acceleration until halfway, then deceleration
-    easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+    easeInOutQuart: function (t: number) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
     // accelerating from zero velocity
-    easeInQuint: function (t) { return t*t*t*t*t },
+    easeInQuint: function (t: number) { return t*t*t*t*t },
     // decelerating to zero velocity
-    easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+    easeOutQuint: function (t: number) { return 1+(--t)*t*t*t*t },
     // acceleration until halfway, then deceleration 
-    easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+    easeInOutQuint: function (t: number) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
 }
 
 interface IHSLColor {
@@ -46,13 +46,24 @@ interface ISun extends IPoint2D {
     strength: number;
 }
 
+interface IColorStep {
+    at: number,
+    use: IHSLColor,
+}
+
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 
 const nightColor: IHSLColor = { h: 0, s: 0, l: 0 };
-const sunriseColor: IHSLColor = { h: 359, s: 66, l: 76 };
+const sunriseColor: IHSLColor = { h: 359, s: 90, l: 43 };
 const midDayColor: IHSLColor = { h: 359, s: 0, l: 100 };
-const sunsetColor: IHSLColor = { h: 360, s: 90, l: 43 };
+const sunsetColor: IHSLColor = { h: 360, s: 66, l: 76 };
+
+const colorSteps = [
+    { at: 0, use: sunriseColor },
+    { at: 640, use: midDayColor },
+    { at: 1000, use: sunsetColor },
+];
 
 const minutesInADay = 1840;
 const referenceHeight = 150;
@@ -62,19 +73,22 @@ const maxDistance = getDistanceBetweenTwoPoints({ x: 0, y: referenceHeight }, { 
 window.requestAnimationFrame(draw);
 
 let titty = 0;
+let fnTitty = (t: number) => t + 1;
 
 let ledArray = new Array(94).fill(0).map<IColoredPoint2D>((x, i) => ({ x: i * 8 + 550, y: referenceHeight / 3, fill: `hsl(360, 0%, 0%)` }));
 
 function draw() {
-    const t = titty++;
+    titty = fnTitty(titty);
+    const t = titty;
     const sun: ISun = getSunPosition(t);
     sun.strength = getNormalizedSunStrength(sun);
+    const sunColor = getSunColor(t, colorSteps);
 
     ctx.clearRect(0, 0, minutesInADay, referenceHeight);
     ctx.fillStyle = 'darkblue';
     ctx.fillRect(0, 0, minutesInADay, referenceHeight);
-    drawSun(sun);
-    ledArray = computeLedArray(ledArray, sun);
+    drawSun(sun, sunColor);
+    ledArray = computeLedArray(ledArray, sun, sunColor);
     drawLeds(ledArray);
 
     if (titty > minutesInADay) {
@@ -95,9 +109,9 @@ function drawLeds(leds: IColoredPoint2D[]) {
     });
 }
 
-function drawSun(sun: ISun) {
+function drawSun(sun: ISun, sunColor: IHSLColor) {
     ctx.beginPath();
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = `hsl(${sunColor.h},${sunColor.s}%,${sunColor.l}%)`;
     ctx.arc(sun.x, sun.y, 10, 0, 2*Math.PI);
     ctx.fill();
     ctx.closePath();
@@ -136,24 +150,40 @@ function getNormalizedSunStrength(sun: ISun) {
     const t = 1 - Math.min(referenceHeight * 2 / 3, sun.y) / (referenceHeight * 2 / 3);
 
     const pow = Math.min(t * 2, 1.5);
-    console.log(pow);
     return pow;
 }
 
-function computeLedArray(leds: IColoredPoint2D[], sun: ISun) {
+function computeLedArray(leds: IColoredPoint2D[], sun: ISun, sunColor: IHSLColor) {
     return leds.map(led => {
-        return { x: led.x, y: led.y, fill: getHslLedColor(led, sun) };
-    })
+        return { x: led.x, y: led.y, fill: getHslLedColor(led, sun, sunColor) };
+    });
 }
 
 
-function getHslLedColor(led: IColoredPoint2D, sun: ISun): string {
+function getHslLedColor(led: IColoredPoint2D, sun: ISun, sunColor: IHSLColor): string {
     const dist = Math.abs(getDistanceBetweenTwoPoints(sun, led));
-    const invertedDist = ((dist / maxDistance));
+    const normalizedDist = ((dist / maxDistance));
 
-    const l = (1 - invertedDist) * sun.strength;
+    const l = (1 - normalizedDist) * sun.strength;
 
+    const min = 550 / minutesInADay;
+    const max = (550 + 94 * 8) / minutesInADay;
 
-    return `hsl(${360},${100}%,${(l) * 100}%)`;
+    const s = (normalizedDist) / (max - min);
+
+    if (led.x === 550) {
+        console.log(normalizedDist, min, max, s);
+    }
+
+    return `hsl(${sunColor.h},${(1 - s) * 100}%,${(l) * 100}%)`;
 }
 
+function getSunColor(t: number, steps: IColorStep[]) {
+    const step = steps.find((e, i, a) => i < steps.length - 1 ? a[i + 1].at >= t : true);
+
+    return step && step.use || midDayColor;
+}
+
+window.onkeydown = (e: KeyboardEvent) => {
+    fnTitty = e.keyCode === 37 ? n => n - 1 : n => n + 1;
+}
