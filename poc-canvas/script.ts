b45ecb665954082e5a2e78b0cbd8d07caff7a5a2
Fabio Brunori <fabiobunori@hotmail.it>
2019-11-23 19:37:00 +0100

    merge

--- poc-canvas/script.ts
+++ poc-canvas/script.ts
@@ -1,58 +1,55 @@
 const EasingFunctions = {
-  // no easing, no acceleration
-  linear: (t: number) => t,
-  // accelerating from zero velocity
-  easeInQuad: (t: number) => t * t,
-  // decelerating to zero velocity
-  easeOutQuad: (t: number) => t * (2 - t),
-  // acceleration until halfway, then deceleration
-  easeInOutQuad: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
-  // accelerating from zero velocity
-  easeInCubic: (t: number) => t * t * t,
-  // decelerating to zero velocity
-  easeOutCubic: (t: number) => --t * t * t + 1,
-  // acceleration until halfway, then deceleration
-  easeInOutCubic: (t: number) =>
-    t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
-  // accelerating from zero velocity
-  easeInQuart: (t: number) => t * t * t * t,
-  // decelerating to zero velocity
-  easeOutQuart: (t: number) => 1 - --t * t * t * t,
-  // acceleration until halfway, then deceleration
-  easeInOutQuart: (t: number) =>
-    t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t,
-  // accelerating from zero velocity
-  easeInQuint: (t: number) => t * t * t * t * t,
-  // decelerating to zero velocity
-  easeOutQuint: (t: number) => 1 + --t * t * t * t * t,
-  // acceleration until halfway, then deceleration
-  easeInOutQuint: (t: number) =>
-    t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t
+    // no easing, no acceleration
+    linear: (t: number) => t,
+    // accelerating from zero velocity
+    easeInQuad: (t: number) => t * t,
+    // decelerating to zero velocity
+    easeOutQuad: (t: number) => t * (2 - t),
+    // acceleration until halfway, then deceleration
+    easeInOutQuad: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+    // accelerating from zero velocity
+    easeInCubic: (t: number) => t * t * t,
+    // decelerating to zero velocity
+    easeOutCubic: (t: number) => --t * t * t + 1,
+    // acceleration until halfway, then deceleration
+    easeInOutCubic: (t: number) =>
+        t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+    // accelerating from zero velocity
+    easeInQuart: (t: number) => t * t * t * t,
+    // decelerating to zero velocity
+    easeOutQuart: (t: number) => 1 - --t * t * t * t,
+    // acceleration until halfway, then deceleration
+    easeInOutQuart: (t: number) =>
+        t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t,
+    // accelerating from zero velocity
+    easeInQuint: (t: number) => t * t * t * t * t,
+    // decelerating to zero velocity
+    easeOutQuint: (t: number) => 1 + --t * t * t * t * t,
+    // acceleration until halfway, then deceleration
+    easeInOutQuint: (t: number) =>
+        t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t
 };
 
+const referenceHeight = 150;
+let minutesInADay = 1840;
 interface IHSLColor {
-  h: number;
-  s: number;
-  l: number;
+    h: number;
+    s: number;
+    l: number;
 }
 
 interface IPoint2D {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 }
 
-interface Hsl {
-  h: number;
-  s: number;
-  l: number;
-}
 
 interface IColoredPoint2D extends IPoint2D {
-  hsl: Hsl;
+    fill: string
 }
 
 interface ISun extends IPoint2D {
-  strength: number;
+    strength: number;
 }
 
 interface IColorStep {
@@ -74,19 +71,18 @@ const colorSteps = [
     { at: 1000, use: sunsetColor },
 ];
 
-  const maxDistance = getDistanceBetweenTwoPoints(
+const maxDistance = getDistanceBetweenTwoPoints(
     { x: 0, y: referenceHeight },
     { x: minutesInADay, y: referenceHeight / 3 }
-  );
+);
 
-  window.requestAnimationFrame(draw);
-
-  let titty = 0;
+window.requestAnimationFrame(draw);
 
 let titty = 0;
+
 let fnTitty = (t: number) => t + 1;
 
-let ledArray = new Array(94).fill(0).map<IColoredPoint2D>((x, i) => ({ x: i * 8 + 550, y: referenceHeight / 3, fill: `hsl(360, 0%, 0%)` }));
+let ledArray = new Array(94).fill(0).map<IColoredPoint2D>((x, i) => ({ x: i * 8 + 550, y: referenceHeight / 3, fill: toHslString({ h: 360, s: 0, l: 0 }) }));
 
 function draw() {
     titty = fnTitty(titty);
@@ -103,47 +99,48 @@ function draw() {
     drawLeds(ledArray);
 
     if (titty > minutesInADay) {
-      titty = 0;
+        titty = 0;
     }
     window.requestAnimationFrame(draw);
-  }
+}
 
-  function drawLeds(leds: IColoredPoint2D[]) {
+function drawLeds(leds: IColoredPoint2D[]) {
     const h = referenceHeight / 3;
 
     leds.forEach(led => {
-      ctx.beginPath();
-      ctx.fillStyle = toHslString(led.hsl);
-      ctx.fillRect(led.x - 1, 150, 2, - led.hsl.l * 150);
-      ctx.arc(led.x, h, 2, 0, 2 * Math.PI);
-      ctx.fill();
-      ctx.closePath();
+        ctx.beginPath();
+        ctx.fillStyle = led.fill;
+        ctx.arc(led.x, h, 2, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
     });
-  }
+}
 
 function drawSun(sun: ISun, sunColor: IHSLColor) {
     ctx.beginPath();
     ctx.fillStyle = `hsl(${sunColor.h},${sunColor.s}%,${sunColor.l}%)`;
-    ctx.arc(sun.x, sun.y, 10, 0, 2*Math.PI);
+    ctx.arc(sun.x, sun.y, 10, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
-  }
-
-  function getDistanceBetweenTwoPoints(a: IPoint2D, b: IPoint2D) {
+}
+function toHslString(sunColor: IHSLColor) {
+    return `hsl(${sunColor.h},${sunColor.s}%,${sunColor.l}%)`
+}
+function getDistanceBetweenTwoPoints(a: IPoint2D, b: IPoint2D) {
     const pit = Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2);
 
     return Math.sqrt(pit);
-  }
+}
 
-  function getSunPosition(t: number) {
+function getSunPosition(t: number) {
     const x = t; // <-- to parametrize
 
     const y = getSunY(normalizeSunX(x));
 
     return { x, y, strength: 0 };
-  }
+}
 
-  function normalizeSunX(x: number) {
+function normalizeSunX(x: number) {
     const min = 0;
     const max = canvas.getBoundingClientRect().width;
 
@@ -151,21 +148,21 @@ function drawSun(sun: ISun, sunColor: IHSLColor) {
     const translated = x - min;
 
     return translated / diff;
-  }
+}
 
-  function getSunY(t: number) {
+function getSunY(t: number) {
     const v = Math.abs((t - 0.5) * 2);
     return EasingFunctions.easeInOutQuad(v) * referenceHeight;
-  }
+}
 
-  function getNormalizedSunStrength(sun: ISun) {
+function getNormalizedSunStrength(sun: ISun) {
     const t =
-      1 -
-      Math.min((referenceHeight * 2) / 3, sun.y) / ((referenceHeight * 2) / 3);
+        1 -
+        Math.min((referenceHeight * 2) / 3, sun.y) / ((referenceHeight * 2) / 3);
 
     const pow = Math.min(t * 2, 1.5);
     return pow;
-  }
+}
 
 function computeLedArray(leds: IColoredPoint2D[], sun: ISun, sunColor: IHSLColor) {
     return leds.map(led => {
